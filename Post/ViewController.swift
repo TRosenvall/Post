a432b393e5c353d78a5cc2a7fc89267814694f27
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-06-24 13:58:59 -0600

    first commit

---
+++ Post/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Post
+//
+//  Created by Timothy Rosenvall on 6/24/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
